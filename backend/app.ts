7dc98ae771889988752809d96ffdc8ba6b72799e
Zy5879 <ZaireM21@gmail.com>
2023-04-05 19:21:09 -0400

    added morgan middleware to app

--- backend/app.ts
+++ backend/app.ts
@@ -3,9 +3,11 @@ import { homeRouter } from "./controllers/home";
 import { productsRouter } from "./controllers/products";
 import { unknownEndpoint } from "./utils/middleware";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/", homeRouter);
