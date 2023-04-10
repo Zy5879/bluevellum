cbc82e6c06fd198f1207784149760cd950aa6e29
Zy5879 <ZaireM21@gmail.com>
2023-04-10 11:57:16 -0400

    created all my get routes for my products

--- backend/controllers/products.ts
+++ backend/controllers/products.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import data from "../data/mockData";
-// import productService from "../services/leatherService";
 export const productsRouter = Router();
 
 productsRouter.get("/bags", (_req, res) => {
@@ -9,20 +8,50 @@ productsRouter.get("/bags", (_req, res) => {
   });
   res.json(item);
 });
-productsRouter.get("/wallets", (_req, res) => {
+productsRouter.get("/bags/gentlemanbag", (_req, res) => {
   const item = data.filter((obj) => {
-    return obj.category === "wallet";
+    return obj.category === "gentlemanbag";
   });
   res.json(item);
 });
-productsRouter.get("/wallets/:id", (req, res) => {
+productsRouter.get("/bags/gentlemanbag/:id", (req, res) => {
   const item = data.filter((obj) => {
-    return obj.category === "wallet";
+    return obj.category === "gentlemanbag";
   });
   const id = Number(req.params.id);
-  const findWalletId = item.find((w) => w.id === id);
-  res.json(findWalletId);
+  const findId = item.find((g) => g.id === id);
+  res.json(findId);
 });
+productsRouter.get("/bags/tote", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "tote";
+  });
+  res.json(item);
+});
+productsRouter.get("/bags/tote/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "tote";
+  });
+  const id = Number(req.params.id);
+  const findId = item.find((t) => t.id === id);
+  res.json(findId);
+});
+productsRouter.get("/bags/handbag", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "handbag";
+  });
+  res.json(item);
+});
+productsRouter.get("/bags/handbag/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "handbag";
+  });
+  const id = Number(req.params.id);
+  const findId = item.find((t) => t.id === id);
+  res.json(findId);
+});
+
+//END OF BAG ROUTES
 
 productsRouter.get("/accessories", (_req, res) => {
   const item = data.filter((obj) => {
@@ -30,10 +59,64 @@ productsRouter.get("/accessories", (_req, res) => {
   });
   res.json(item);
 });
-// productsRouter.get("/customs", (_req, res) => {
-//   const customs = data.map((item) => item.bags.customBags);
-//   res.json(customs);
-// });
-// productsRouter.get("/allProducts", (_req, res) => {
-//   res.json(data);
-// });
+productsRouter.get("/accessories/wallets", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "wallet";
+  });
+  res.json(item);
+});
+productsRouter.get("/accessories/wallets/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "wallet";
+  });
+  const id = Number(req.params.id);
+  const findWalletId = item.find((w) => w.id === id);
+  res.json(findWalletId);
+});
+productsRouter.get("/accessories/belt", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "belt";
+  });
+  res.json(item);
+});
+productsRouter.get("/accessories/belt/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "belt";
+  });
+  const id = Number(req.params.id);
+  const findId = item.find((b) => b.id === id);
+  res.json(findId);
+});
+productsRouter.get("/accessories/watch", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "watch";
+  });
+  res.json(item);
+});
+productsRouter.get("/accessories/watch/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "watch";
+  });
+  const id = Number(req.params.id);
+  const findId = item.find((wtc) => wtc.id === id);
+  res.json(findId);
+});
+
+//END OF ACCESSORIES ROUTES
+
+productsRouter.get("/customs", (_req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "custom";
+  });
+  res.json(item);
+});
+productsRouter.get("/customs/:id", (req, res) => {
+  const item = data.filter((obj) => {
+    return obj.category === "custom";
+  });
+  const id = Number(req.params.id);
+  const findId = item.filter((cus) => cus.id === id);
+  res.json(findId);
+});
+
+//END OF CUSTOM ROUTES
