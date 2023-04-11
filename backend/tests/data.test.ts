3a5c28b5c7cdae549f9ccc11342702ab59114085
Zy5879 <ZaireM21@gmail.com>
2023-04-11 19:39:01 -0400

    testing routes in backend

--- backend/tests/data.test.ts
+++ backend/tests/data.test.ts
@@ -19,9 +19,9 @@ describe("test routes", () => {
     expect(res.body).toEqual(accesories);
   });
   test("get wallet data", async () => {
-    const res = await request(app).get("/products/accessories/wallets");
+    const res = await request(app).get("/products/wallets");
     const wallets = data.filter((item) => {
-      return item.category === "wallet";
+      return item.type === "wallet";
     });
     expect(res.body).toEqual(wallets);
   });
@@ -31,5 +31,43 @@ describe("test routes", () => {
       return item.category === "custom";
     });
     expect(res.body).toEqual(customs);
+  });
+});
+
+describe("test routes by id", () => {
+  test("get tote data by id", async () => {
+    const res = await request(app).get("/products/bags/tote/10");
+    const tote = data.find((d) => d.id === 10);
+    expect(res.body).toEqual(tote);
+  });
+  test("get wallet by id", async () => {
+    const res = await request(app).get("/products/wallets/14");
+    const wallet = data.find((d) => d.id === 14);
+    expect(res.body).toEqual(wallet);
+  });
+  test("get belt by id", async () => {
+    const res = await request(app).get("/products/accessories/belt/23");
+    const belt = data.find((d) => d.id === 23);
+    expect(res.body).toEqual(belt);
+  });
+  test("get watchband by id", async () => {
+    const res = await request(app).get("/products/accessories/watch/12");
+    const watch = data.find((d) => d.id === 12);
+    expect(res.body).toEqual(watch);
+  });
+  test("get gentlemanbag by id", async () => {
+    const res = await request(app).get("/products/bags/gentlemanbag/1");
+    const gb = data.find((d) => d.id === 1);
+    expect(res.body).toEqual(gb);
+  });
+  test("get handbag by id", async () => {
+    const res = await request(app).get("/products/bags/handbag/3");
+    const hb = data.find((d) => d.id === 3);
+    expect(res.body).toEqual(hb);
+  });
+  test("get custom by id", async () => {
+    const res = await request(app).get("/products/customs/9");
+    const hb = data.find((d) => d.id === 9);
+    expect(res.body).toEqual([hb]);
   });
 });
