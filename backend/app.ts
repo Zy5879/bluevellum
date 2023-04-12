dc219744e3f3e65621004c26ff046003828392dc
Zy5879 <ZaireM21@gmail.com>
2023-04-11 22:33:53 -0400

    added mongoose to backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,27 @@
+import config from "./utils/config";
+// import * as dotenv from "dotenv";
+// dotenv.config();
 import express from "express";
 import { productsRouter } from "./controllers/products";
 import { errorHandler, unknownEndpoint } from "./utils/middleware";
 import cors from "cors";
 import morgan from "morgan";
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+console.log("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(`${config.MONGODB_URI}`)
+  .then(() => {
+    console.log("connected to MONGODB");
+  })
+  .catch((error) => {
+    if (error instanceof Error) {
+      console.log("error connecting to MONGODB", error.message);
+    }
+  });
 
 const app = express();
 app.use(cors());
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -2,8 +2,16 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export = PORT;
+const config = {
+  PORT,
+  MONGODB_URI,
+};
+
+export default config;
+
+// export = PORT;
 // module.exports = {
 //   PORT,
 // };
