ec2aaf377b83ff11679fd60fe733b46b77b928e4
Zy5879 <ZaireM21@gmail.com>
2023-05-17 16:19:20 -0400

    made changes

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
