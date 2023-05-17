c9757f56cf3e8ecfa2d03bc197e3d8681c7e93f7
Zy5879 <ZaireM21@gmail.com>
2023-05-17 18:41:12 -0400

    changed some env

--- frontend/src/redux/features/productApi.ts
+++ frontend/src/redux/features/productApi.ts
@@ -16,67 +16,44 @@ export const productApi = createApi({
     getProductId: builder.query<LeatherInfo, string>({
       query: (id) => `products/item/${id}`,
     }),
-    // getBagsGbId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `bags/gentlemanbag/${id}`,
-    // }),
+
     getBagsTote: builder.query<LeatherInfo[], void>({
       query: () => "products/bags/tote",
     }),
-    // getBagsToteId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `bags/tote${id}`,
-    // }),
+
     getBagsHb: builder.query<LeatherInfo[], void>({
       query: () => "products/bags/handbag",
     }),
-    // getBagsHbId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `bag/handbag/${id}`,
-    // }),
+
     getWallets: builder.query<LeatherInfo[], void>({
       query: () => "products/wallets",
     }),
-    // getWalletsId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `wallets/${id}`,
-    // }),
+
     getAccess: builder.query<LeatherInfo[], void>({
       query: () => "products/accessories",
     }),
     getAccessBelt: builder.query<LeatherInfo[], void>({
       query: () => "products/accessories/belt",
     }),
-    // getAccessBeltId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `accessories/belt/${id}`,
-    // }),
+
     getAcessWatch: builder.query<LeatherInfo[], void>({
       query: () => "products/accessories/watch",
     }),
-    // getAcessWatchId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `accessories/watch/${id}`,
-    // }),
     getCustoms: builder.query<LeatherInfo[], void>({
       query: () => "products/customs",
     }),
-    // getCustomsId: builder.query<LeatherInfo[], string>({
-    //   query: (id) => `customs/${id}`,
-    // }),
   }),
 });
 
 export const {
-  // useGetAccessBeltIdQuery,
   useGetAccessBeltQuery,
   useGetAccessQuery,
-  // useGetAcessWatchIdQuery,
   useGetAcessWatchQuery,
   useGetBagGbQuery,
-  // useGetBagsGbIdQuery,
-  // useGetBagsHbIdQuery,
   useGetBagsHbQuery,
   useGetBagsQuery,
-  // useGetBagsToteIdQuery,
   useGetBagsToteQuery,
-  // useGetCustomsIdQuery,
   useGetCustomsQuery,
-  // useGetWalletsIdQuery,
   useGetWalletsQuery,
   useGetProductIdQuery,
 } = productApi;
