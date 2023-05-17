e9a939d3608ab26b64a9707ee3065541c8a8c3fb
Zy5879 <ZaireM21@gmail.com>
2023-05-17 16:37:30 -0400

    changed homerouter

--- backend/build/backend/controllers/home.js
+++ backend/build/backend/controllers/home.js
@@ -1,33 +1,9 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.homeRouter = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const express_1 = require("express");
-const express_async_handler_1 = __importDefault(require("express-async-handler"));
-const user_1 = __importDefault(require("../models/user"));
-const middleware_1 = require("../utils/middleware");
 exports.homeRouter = (0, express_1.Router)();
-exports.homeRouter.get("/", (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const token = (0, middleware_1.getTokenFrom)(req);
-    const decodedToken = jsonwebtoken_1.default.verify(token, `${process.env.SECRET}`);
-    const user = yield user_1.default.findById(decodedToken.id).populate("cart");
-    if (!user) {
-        return res.status(404).json({ error: "No User Found" }).end();
-    }
-    else {
-        return res.json(user);
-    }
-})));
+exports.homeRouter.get("/", (_req, res) => {
+    res.send("Welcome to Blue Vellum");
+});
 exports.default = exports.homeRouter;
--- backend/controllers/home.ts
+++ backend/controllers/home.ts
@@ -1,30 +1,9 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { Router } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
-import asyncHandler from "express-async-handler";
-import { IUser } from "../types";
-import User from "../models/user";
-import { getTokenFrom } from "../utils/middleware";
 
 export const homeRouter = Router();
 
-homeRouter.get<ParamsDictionary, any, IUser>(
-  "/",
-  asyncHandler(async (req, res): Promise<any> => {
-    const token = getTokenFrom(req) as string;
-    const decodedToken = jwt.verify(
-      token,
-      `${process.env.SECRET}`
-    ) as JwtPayload;
-
-    const user = await User.findById(decodedToken.id).populate("cart");
-
-    if (!user) {
-      return res.status(404).json({ error: "No User Found" }).end();
-    } else {
-      return res.json(user);
-    }
-  })
-);
+homeRouter.get("/", (_req, res) => {
+  res.send("Welcome to Blue Vellum");
+});
 
 export default homeRouter;
